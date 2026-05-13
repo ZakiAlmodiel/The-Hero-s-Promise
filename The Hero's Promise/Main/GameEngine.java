713254ee41e8edb316274d36c06450f1ed94af60
Zaki Almodiel <aveenzaki8@gmail.com>
2026-05-13 20:00:32 +0800

    Game Engine plan

--- The Hero's Promise/Main/GameEngine.java
+++ The Hero's Promise/Main/GameEngine.java
@@ -1,13 +1,41 @@
 package Main;
-import Heroes.Hero;
+import Heroes.*;
+import javax.swing.*;
 
 public class GameEngine implements Runnable {
+    private final GameGUI gui;
+    private Thread gameThread;
+    private Hero currentHero;
+    
+    public GameEngine(GameGUI gui) {
+        this.gui = gui;
+    }
+    
     public void start() {
-        System.out.println("Game Engine starting...");
+        gameThread = new Thread(this, "GameEngine");
+        gameThread.start();
+    }
+    
+    private void selectHero() {
+        String[] heroes = {"Kyle", "Zaki", "Kurt", "Joshua", "Adrian"};
+        int choice = JOptionPane.showOptionDialog(gui, "Choose your hero", "Hero Selection",
+            JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, heroes, heroes[0]);
+        
+        switch(choice) {
+            case 0: currentHero = new Kyle("Kyle"); break;
+            case 1: currentHero = new Zaki("Zaki"); break;
+            case 2: currentHero = new Kurt("Kurt"); break;
+            case 3: currentHero = new Joshua("Joshua"); break;
+            case 4: currentHero = new Adrian("Adrian"); break;
+            default: currentHero = new Kyle("Kyle");
+        }
+        
+        gui.appendToTerminal("Selected: " + currentHero.getHeroName() + "\n");
     }
 
     @Override
     public void run() {
-        System.out.println("Game Engine running...");
+        selectHero();
+        gui.appendToTerminal("\nGame starting...\n");
     }
 }
