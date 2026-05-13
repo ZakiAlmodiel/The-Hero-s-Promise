75aba09885633f1006d31452d040d3c540de81a3
Shinonaguwa <shizusama2006@gmail.com>
2026-05-13 23:05:21 +0800

    details class implementation partial progress

--- The Hero's Promise/Story/Details.java
+++ The Hero's Promise/Story/Details.java
@@ -3,7 +3,21 @@ import Enemy.Enemy;
 import Heroes.Hero;
 
 public class Details {
+    public static void displayPickingDetails(Hero hero) {
+        System.out.println();
+        System.out.println("--------------------------------------------------");
+        System.out.println("You've picked " + hero.getHeroName());
+        System.out.println("HP: " + hero.getHeroHp() + "/" + hero.getMaxHp());
+        System.out.println("Mana: " + hero.getHeroMana() + "/" + hero.getMaxMana());
+        System.out.println();
+    }
+    
     public static void displayCurrentDetails(Hero hero, Enemy enemy) {
-        System.out.println("Displaying battle details...");
+        System.out.println();
+        System.out.println("--------------------------------------------------");
+        System.out.println("HERO: " + hero.getHeroName() + " | ENEMY: " + enemy.getEnemyName());
+        System.out.println("HP: " + hero.getHeroHp() + " | HP: " + enemy.getEnemyHp());
+        System.out.println("MP: " + hero.getHeroMana() + " | MP: " + enemy.getEnemyMana());
+        System.out.println("--------------------------------------------------");
     }
 }
