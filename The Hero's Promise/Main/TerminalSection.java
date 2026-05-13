c9d838177cd9d10d4abc1b24f33258ffd257c07f
Zaki Almodiel <aveenzaki8@gmail.com>
2026-05-13 20:01:12 +0800

    terminal section added codes

--- The Hero's Promise/Main/TerminalSection.java
+++ The Hero's Promise/Main/TerminalSection.java
@@ -1,12 +1,38 @@
 package Main;
 import javax.swing.*;
+import java.awt.*;
 
 public class TerminalSection extends JPanel {
-    private JTextArea terminalArea;
+    private JTextPane terminalPane;
+    private JScrollPane scrollPane;
+    private JPanel buttonPanel;
     
     public TerminalSection() {
-        terminalArea = new JTextArea();
-        terminalArea.setText("Terminal output will appear here.");
-        add(terminalArea);
+        setLayout(new BorderLayout());
+        setBackground(new Color(0, 0, 0, 0));
+        
+        terminalPane = new JTextPane();
+        terminalPane.setEditable(false);
+        terminalPane.setBackground(Color.BLACK);
+        terminalPane.setForeground(new Color(220, 190, 130));
+        terminalPane.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        
+        scrollPane = new JScrollPane(terminalPane);
+        scrollPane.setOpaque(false);
+        
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        buttonPanel.setOpaque(false);
+        buttonPanel.setPreferredSize(new Dimension(0, 60));
+        
+        add(scrollPane, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.SOUTH);
+    }
+    
+    public JTextPane getTerminalPane() {
+        return terminalPane;
+    }
+    
+    public JPanel getButtonPanel() {
+        return buttonPanel;
     }
 }
