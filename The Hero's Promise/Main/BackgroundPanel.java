32ed6458baf99f99b91eb24daae90a93ba1b8115
Shinonaguwa <shizusama2006@gmail.com>
2026-05-13 23:06:13 +0800

    adjustments background panel color

--- The Hero's Promise/Main/BackgroundPanel.java
+++ The Hero's Promise/Main/BackgroundPanel.java
@@ -1,16 +1,29 @@
 package Main;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class BackgroundPanel extends JPanel {
+    private BufferedImage image;
+    
     public BackgroundPanel() {
-        setBackground(Color.BLACK);
+        setOpaque(true);
+        setBackground(new Color(10, 8, 5));
+    }
+    
+    public void setImage(BufferedImage img) {
+        this.image = img;
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.BLACK);
-        g.fillRect(0, 0, getWidth(), getHeight());
+        if (image != null) {
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        } else {
+            g.setColor(new Color(10, 8, 5));
+            g.fillRect(0, 0, getWidth(), getHeight());
+        }
     }
 }
