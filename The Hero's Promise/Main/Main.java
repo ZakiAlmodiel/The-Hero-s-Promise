90fa6caa5b64bc9c2a890c12ff4d55b991d2ff8b
Zaki Almodiel <aveenzaki8@gmail.com>
2026-05-13 19:59:52 +0800

    updated main draft 2

--- The Hero's Promise/Main/Main.java
+++ The Hero's Promise/Main/Main.java
@@ -2,7 +2,13 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The Hero's Promise - Alpha");
-        System.out.println("Work in progress...");
+        System.out.println("The Hero's Promise - Alpha v0.2");
+        System.out.println("Loading game...");
+        
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            GameGUI gui = new GameGUI();
+            GameEngine engine = new GameEngine(gui);
+            engine.start();
+        });
     }
 }
