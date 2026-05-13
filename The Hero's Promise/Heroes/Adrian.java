cef354196578524b88990add8593fe0100331218
Shinonaguwa <shizusama2006@gmail.com>
2026-05-13 23:04:31 +0800

    adrian class draft partial progression

--- The Hero's Promise/Heroes/Adrian.java
+++ The Hero's Promise/Heroes/Adrian.java
@@ -6,17 +6,31 @@ public class Adrian extends Hero {
     }
 
     @Override
+    public void displayBackStory() {
+        System.out.println("Adrian's backstory - arcane mage from destroyed village.");
+    }
+
+    @Override
+    public void displaySkillOptions() {
+        System.out.println("Skill 1: Arcane Bolt");
+        System.out.println("Skill 2: Mana Barrier");
+        System.out.println("Skill 3: Fireball");
+        System.out.println("Skill 4: Heal HP");
+        System.out.println("Skill 5: Heal Mana");
+    }
+
+    @Override
     public void preBattleDialogue() {
-        System.out.printf("Adrian speaks.\n");
+        System.out.printf("\nAdrian: The arcane answers my call.\n");
     }
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("Adrian wins.\n");
+        System.out.printf("\nAdrian: Predictable.\n");
     }
 
     @Override
     public void defeatDialogue() {
-        System.out.printf("Adrian loses.\n");
+        System.out.printf("\nAdrian: My studies... incomplete...\n");
     }
 }
