cc5a69787a7be46f40604e8c5d0559b937219a26
Shinonaguwa <shizusama2006@gmail.com>
2026-05-14 00:37:15 +0800

    organized some descriptions

--- The Hero's Promise/Heroes/Adrian.java
+++ The Hero's Promise/Heroes/Adrian.java
@@ -7,16 +7,18 @@ public class Adrian extends Hero {
 
     @Override
     public void displayBackStory() {
-        System.out.println("Adrian's backstory - arcane mage from destroyed village.");
+        System.out.println("-----------------BACKSTORY-------------------");
+        System.out.println("Adrian was born with an innate connection to the arcane.");
+        System.out.println("After his village was razed, he embraced his power fully.");
     }
 
     @Override
     public void displaySkillOptions() {
-        System.out.println("Skill 1: Arcane Bolt");
-        System.out.println("Skill 2: Mana Barrier");
-        System.out.println("Skill 3: Fireball");
-        System.out.println("Skill 4: Heal HP");
-        System.out.println("Skill 5: Heal Mana");
+        System.out.println("Skill 1 (Arcane Bolt) | 10 damage | 10 Mana");
+        System.out.println("Skill 2 (Mana Barrier) | 15 damage | 15 Mana");
+        System.out.println("Skill 3 (Fireball) | 20 damage | 20 Mana");
+        System.out.println("Skill 4 (Heal HP)");
+        System.out.println("Skill 5 (Heal Mana)");
     }
 
     @Override
@@ -26,7 +28,7 @@ public class Adrian extends Hero {
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("\nAdrian: Predictable.\n");
+        System.out.printf("\nAdrian: A predictable outcome.\n");
     }
 
     @Override
