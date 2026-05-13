ae8b1dcded4236190c4e51383e790da6cd00b3b5
Zaki Almodiel <aveenzaki8@gmail.com>
2026-05-13 20:17:53 +0800

    3rd draft main.java

--- The Hero's Promise/Main/Main.java
+++ The Hero's Promise/Main/Main.java
@@ -2,8 +2,11 @@ package Main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The Hero's Promise - Alpha v0.2");
+        System.out.println("The Hero's Promise - Alpha v0.3");
         System.out.println("Loading game...");
+        System.out.println("Features: Basic battle system, 3 levels, 5 heroes");
+        
+        TerminalMusic.playMusic();
         
         javax.swing.SwingUtilities.invokeLater(() -> {
             GameGUI gui = new GameGUI();
