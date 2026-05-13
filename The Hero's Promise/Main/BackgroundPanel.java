a8ca3287b03093d8085ab128233c5e531e779787
Shinonaguwa <shizusama2006@gmail.com>
2026-05-14 00:41:00 +0800

    revised some stuff for further adjustments

--- The Hero's Promise/Main/BackgroundPanel.java
+++ The Hero's Promise/Main/BackgroundPanel.java
@@ -2,6 +2,8 @@ package Main;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
 
 public class BackgroundPanel extends JPanel {
     private BufferedImage image;
@@ -15,12 +17,26 @@ public class BackgroundPanel extends JPanel {
         this.image = img;
         repaint();
     }
+    
+    public BufferedImage loadBackground(String name) {
+        try {
+            String path = "Images/" + name + ".png";
+            File f = new File(path);
+            if (f.exists()) {
+                return ImageIO.read(f);
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to load background: " + name);
+        }
+        return null;
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         if (image != null) {
-            g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+            Graphics2D g2 = (Graphics2D) g;
+            g2.drawImage(image, 0, 0, getWidth(), getHeight(), null);
         } else {
             g.setColor(new Color(10, 8, 5));
             g.fillRect(0, 0, getWidth(), getHeight());
