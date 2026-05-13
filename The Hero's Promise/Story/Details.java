c9a469d86209e90a343c990c0f7cde68b3e3eb0e
Shinonaguwa <shizusama2006@gmail.com>
2026-05-13 22:43:25 +0800

    story details draft - might update it later

--- The Hero's Promise/Story/Details.java
+++ The Hero's Promise/Story/Details.java
@@ -1,24 +1,9 @@
 package Story;
-
 import Enemy.Enemy;
 import Heroes.Hero;
 
 public class Details {
-    public static void displayPickingDetails(Hero hero) {
-        System.out.println();
-        System.out.println("--------------------------------------------------");
-        System.out.println("You've picked " + hero.getHeroName());
-        System.out.println("HP: " + hero.getHeroHp() + "/" + hero.getMaxHp());
-        System.out.println("Mana: " + hero.getHeroMana() + "/" + hero.getMaxMana());
-        System.out.println();
-    }
-
     public static void displayCurrentDetails(Hero hero, Enemy enemy) {
-        System.out.println();
-        System.out.println("--------------------------------------------------");
-        System.out.printf("NAMES:%15s%25s\n", hero.getHeroName(), enemy.getEnemyName());
-        System.out.printf("HP:%17d %22d\n", hero.getHeroHp(), enemy.getEnemyHp());
-        System.out.printf("MP:%17d %22d\n", hero.getHeroMana(), enemy.getEnemyMana());
-        System.out.println();
+        System.out.println("Displaying battle details...");
     }
 }
