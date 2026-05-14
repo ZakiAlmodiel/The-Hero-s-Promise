a764b9f37c46406e15885f96ebadf381bb24ecca
Joshua Raagas <joshuaephraim.raagas@cit.edu>
2026-05-14 23:33:41 +0800

    Add files via upload

--- The Hero's Promise/Main/TerminalMusic.java
+++ The Hero's Promise/Main/TerminalMusic.java
@@ -1,26 +1,11 @@
 package Main;
-import java.io.File;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 
 public class TerminalMusic {
-    private static Clip musicClip;
-
     public static void playMusic() {
-        try {
-            File musicFile = new File("G8Game/Main/Terminalmusic.wav");
-            musicClip = AudioSystem.getClip();
-            musicClip.open(AudioSystem.getAudioInputStream(musicFile));
-            musicClip.loop(Clip.LOOP_CONTINUOUSLY);
-            musicClip.start();
-        } catch (Exception e) {
-            System.out.println("Error playing music: " + e.getMessage());
-        }
+        System.out.println("Music placeholder.");
     }
 
     public static void stopMusic() {
-        if (musicClip != null && musicClip.isRunning()) {
-            musicClip.stop();
-        }
+        System.out.println("Music stopped.");
     }
 }
