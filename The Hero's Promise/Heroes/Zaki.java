8ee73c6a6d89d713e57207ef16433fac103fa826
Zaki Almodiel <aveenzaki8@gmail.com>
2026-05-14 20:34:37 +0800

    more zaki story

--- The Hero's Promise/Heroes/Zaki.java
+++ The Hero's Promise/Heroes/Zaki.java
@@ -1,38 +1,46 @@
 package Heroes;
 
 public class Zaki extends Hero {
+    private static final int DAMAGE_SKILL1 = 20;
+    private static final int DAMAGE_SKILL2 = 10;
+    private static final int DAMAGE_SKILL3 = 25;
+    private static final int MANA_COST1 = 8;
+    private static final int MANA_COST2 = 6;
+    private static final int MANA_COST3 = 22;
+
     public Zaki(String name) {
-        super(name);
+        super(name, "Phantom Dagger", "Smoke Bomb", "Shadow Fury", DAMAGE_SKILL1, DAMAGE_SKILL2, DAMAGE_SKILL3, MANA_COST1, MANA_COST2, MANA_COST3);
     }
 
     @Override
     public void displayBackStory() {
         System.out.println("-----------------BACKSTORY-------------------");
-        System.out.println("Zaki is a prince whose kingdom was attacked by invaders.");
-        System.out.println("Now his name is whispered in hope among survivors.");
+        System.out.println();
+        System.out.println("Zaki was a prince whose kingdom was attacked by invaders.");
+        System.out.println("Forced to flee, he survived by his wits and stealth, vowing to grow strong enough to reclaim his throne.");
     }
 
     @Override
     public void displaySkillOptions() {
-        System.out.println("Skill 1 (Phantom Dagger) | 10 damage | 10 Mana");
-        System.out.println("Skill 2 (Smoke Bomb) | 15 damage | 15 Mana");
-        System.out.println("Skill 3 (Shadow Fury) | 20 damage | 20 Mana");
-        System.out.println("Skill 4 (Heal HP)");
-        System.out.println("Skill 5 (Heal Mana)");
+        System.out.println("Skill 1 (Phantom Dagger) | " + getSkillOneMultiplier() + " damage | " + getSkillOneManaCost() + " Mana");
+        System.out.println("Skill 2 (Smoke Bomb) | " + getSkillTwoMultiplier() + " damage | " + getSkillTwoManaCost() + " Mana");
+        System.out.println("Skill 3 (Shadow Fury) | " + getSkillThreeMultiplier() + " damage | " + getSkillThreeManaCost() + " Mana");
+        System.out.println("Skill 4 (Heal HP) - Restores 35 HP");
+        System.out.println("Skill 5 (Heal Mana) - Restores 35 MP");
     }
 
     @Override
     public void preBattleDialogue() {
-        System.out.printf("\nZaki: The shadows are my ally.\n");
+        System.out.printf("\nZaki: The shadows are my ally. You won't see me coming.\n");
     }
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("\nZaki: Another job well done.\n");
+        System.out.printf("\nZaki: Another job well done. One step closer to reclaiming my throne.\n");
     }
 
     @Override
     public void defeatDialogue() {
-        System.out.printf("\nZaki: Not like this...\n");
+        System.out.printf("\nZaki: Not... like this... I have... people counting on me...\n");
     }
 }
