2f564bf32279b34928a376511a232a48a0d15455
Joshua Raagas <joshuaephraim.raagas@cit.edu>
2026-05-15 22:14:26 +0800

    Add files via upload

--- The Hero's Promise/Levels/Level1.java
+++ The Hero's Promise/Levels/Level1.java
@@ -1,10 +1,47 @@
 package Levels;
+import java.util.Scanner;
 import Enemy.Enemy;
 import Heroes.Hero;
 
 public class Level1 {
+    static Scanner sc = new Scanner(System.in);
+
     public static boolean battleMechanic1(Hero hero) {
-        System.out.println("Level 1 started.");
+        String[] monsterNames = {"Shadowblade", "Frostlord", "Demonking"};
+        
+        for (int difficulty = 1; difficulty <= 3; difficulty++) {
+            Enemy enemy = new Enemy(monsterNames[difficulty-1], 40 + (difficulty * 8), 8 + (difficulty * 2));
+            
+            System.out.println("\n--- Battle with " + enemy.getEnemyName() + " ---");
+            
+            while (hero.getHeroHp() > 0 && enemy.getEnemyHp() > 0) {
+                hero.displaySkillOptions();
+                System.out.print("Pick a skill (1-5): ");
+                int skillNum = sc.nextInt();
+                
+                if (skillNum == 1) {
+                    enemy.takeDamage(10);
+                    System.out.println("You attacked!");
+                } else if (skillNum == 4) {
+                    hero.restoreHp(35);
+                    System.out.println("You healed HP!");
+                }
+                
+                if (enemy.getEnemyHp() > 0) {
+                    enemy.enemyAttack(hero);
+                    System.out.println("\nYour HP: " + hero.getHeroHp());
+                }
+            }
+            
+            if (hero.getHeroHp() <= 0) {
+                System.out.println("GAME OVER!");
+                return false;
+            }
+            
+            System.out.println("\nDefeated " + enemy.getEnemyName() + "!");
+        }
+        
+        System.out.println("\n----LEVEL 1 COMPLETE----");
         return true;
     }
 }
