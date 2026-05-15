c38417b35ccdb52e04af4eae37bfffb6d518f3c2
Kyle Rey Arañas <kylearanas04@gmail.com>
2026-05-15 22:45:16 +0800

    Revise Kyle's dialogue and skill descriptions

--- The Hero's Promise/Heroes/Kyle.java
+++ The Hero's Promise/Heroes/Kyle.java
@@ -7,26 +7,28 @@ public class Kyle extends Hero {
 
     @Override
     public void displayBackStory() {
-        System.out.println("Kyle's backstory - exiled royal guard captain.");
+        System.out.println("-----------------BACKSTORY-------------------");
+        System.out.println("Kyle was a loyal captain of the royal guard.");
+        System.out.println("Now he fights for the people crushed beneath the crown.");
     }
 
     @Override
     public void displaySkillOptions() {
-        System.out.println("Skill 1: Steel Strike");
-        System.out.println("Skill 2: Shield Bash");
-        System.out.println("Skill 3: Warbringer");
-        System.out.println("Skill 4: Heal HP");
-        System.out.println("Skill 5: Heal Mana");
+        System.out.println("Skill 1 (Steel Strike) | 10 damage | 10 Mana");
+        System.out.println("Skill 2 (Shield Bash) | 15 damage | 15 Mana");
+        System.out.println("Skill 3 (Warbringer) | 20 damage | 20 Mana");
+        System.out.println("Skill 4 (Heal HP)");
+        System.out.println("Skill 5 (Heal Mana)");
     }
 
     @Override
     public void preBattleDialogue() {
-        System.out.printf("\nKyle: I fight for the powerless.\n");
+        System.out.printf("\nKyle: I fight for those who cannot.\n");
     }
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("\nKyle: Justice served.\n");
+        System.out.printf("\nKyle: Justice has been served.\n");
     }
 
     @Override
