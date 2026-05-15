53996cdb2d0651937479135904dcedad169970ff
Kyle Rey Arañas <kylearanas04@gmail.com>
2026-05-15 22:43:33 +0800

    Enhance Kyle's character dialogues and skills

--- The Hero's Promise/Heroes/Kyle.java
+++ The Hero's Promise/Heroes/Kyle.java
@@ -6,17 +6,31 @@ public class Kyle extends Hero {
     }
 
     @Override
+    public void displayBackStory() {
+        System.out.println("Kyle's backstory - exiled royal guard captain.");
+    }
+
+    @Override
+    public void displaySkillOptions() {
+        System.out.println("Skill 1: Steel Strike");
+        System.out.println("Skill 2: Shield Bash");
+        System.out.println("Skill 3: Warbringer");
+        System.out.println("Skill 4: Heal HP");
+        System.out.println("Skill 5: Heal Mana");
+    }
+
+    @Override
     public void preBattleDialogue() {
-        System.out.printf("Kyle speaks.\n");
+        System.out.printf("\nKyle: I fight for the powerless.\n");
     }
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("Kyle wins.\n");
+        System.out.printf("\nKyle: Justice served.\n");
     }
 
     @Override
     public void defeatDialogue() {
-        System.out.printf("Kyle loses.\n");
+        System.out.printf("\nKyle: I will not fall here...\n");
     }
 }
