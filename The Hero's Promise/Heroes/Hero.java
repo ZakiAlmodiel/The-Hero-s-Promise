89adce75ddce05494af92cd3b738d5fb1d840826
Kyle Rey Arañas <kylearanas04@gmail.com>
2026-05-15 22:43:12 +0800

    Add maxHp and maxMana attributes to Hero class

--- The Hero's Promise/Heroes/Hero.java
+++ The Hero's Promise/Heroes/Hero.java
@@ -4,6 +4,8 @@ import Story.Dialogue;
 
 public abstract class Hero extends Dialogue {
     private String heroName;
+    private int maxHp = 100;
+    private int maxMana = 100;
     private int heroHp = 100;
     private int heroMana = 100;
     
@@ -18,4 +20,44 @@ public abstract class Hero extends Dialogue {
     public int getHeroHp() {
         return heroHp;
     }
+    
+    public int getHeroMana() {
+        return heroMana;
+    }
+    
+    public int getMaxHp() {
+        return maxHp;
+    }
+    
+    public int getMaxMana() {
+        return maxMana;
+    }
+    
+    public void setHeroHp(int hp) {
+        this.heroHp = Math.min(maxHp, Math.max(0, hp));
+    }
+    
+    public void setHeroMana(int mana) {
+        this.heroMana = Math.min(maxMana, Math.max(0, mana));
+    }
+    
+    public void takeDamage(int damage) {
+        this.heroHp = Math.max(0, this.heroHp - damage);
+    }
+    
+    public void restoreHp(int amount) {
+        this.heroHp = Math.min(maxHp, this.heroHp + amount);
+    }
+    
+    public void restoreMana(int amount) {
+        this.heroMana = Math.min(maxMana, this.heroMana + amount);
+    }
+    
+    public void fullRestore() {
+        this.heroHp = maxHp;
+        this.heroMana = maxMana;
+    }
+    
+    public abstract void displayBackStory();
+    public abstract void displaySkillOptions();
 }
