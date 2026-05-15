3e53c8d59db50fde7a6dc06de106ee224ebf23a9
Kyle Rey Arañas <kylearanas04@gmail.com>
2026-05-15 22:42:50 +0800

    Add displayDialogue method to Dialogue class

--- The Hero's Promise/Story/Dialogue.java
+++ The Hero's Promise/Story/Dialogue.java
@@ -4,4 +4,8 @@ public abstract class Dialogue {
     public abstract void preBattleDialogue();
     public abstract void victoryDialogue();
     public abstract void defeatDialogue();
+    
+    public void displayDialogue(String message) {
+        System.out.println(message);
+    }
 }
