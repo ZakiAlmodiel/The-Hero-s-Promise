6ee46887eaabb4cd8549321c2d4ea479d0d0914c
Joshua Raagas <joshuaephraim.raagas@cit.edu>
2026-05-15 22:13:11 +0800

    Add files via upload

--- The Hero's Promise/Heroes/Joshua.java
+++ The Hero's Promise/Heroes/Joshua.java
@@ -6,17 +6,31 @@ public class Joshua extends Hero {
     }
 
     @Override
+    public void displayBackStory() {
+        System.out.println("Joshua's backstory - holy paladin from monastery.");
+    }
+
+    @Override
+    public void displaySkillOptions() {
+        System.out.println("Skill 1: Holy Smite");
+        System.out.println("Skill 2: Divine Shield");
+        System.out.println("Skill 3: Judgment");
+        System.out.println("Skill 4: Heal HP");
+        System.out.println("Skill 5: Heal Mana");
+    }
+
+    @Override
     public void preBattleDialogue() {
-        System.out.printf("Joshua speaks.\n");
+        System.out.printf("\nJoshua: The light shall prevail.\n");
     }
 
     @Override
     public void victoryDialogue() {
-        System.out.printf("Joshua wins.\n");
+        System.out.printf("\nJoshua: Righteous triumph.\n");
     }
 
     @Override
     public void defeatDialogue() {
-        System.out.printf("Joshua loses.\n");
+        System.out.printf("\nJoshua: Forgive... me...\n");
     }
 }
