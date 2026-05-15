968f5ed3f0d993c737e0780d89c2c02c3db3374c
Kyle Rey Arañas <kylearanas04@gmail.com>
2026-05-15 22:42:22 +0800

    Add sound playback methods for button, attack, and heal

--- The Hero's Promise/Main/SoundManager.java
+++ The Hero's Promise/Main/SoundManager.java
@@ -1,7 +1,34 @@
 package Main;
+import java.io.File;
+import javax.sound.sampled.*;
 
 public class SoundManager {
+    
     public static void playSound(String filename) {
-        System.out.println("Playing sound: " + filename);
+        try {
+            File soundFile = new File("Main/" + filename);
+            if (soundFile.exists()) {
+                AudioInputStream ais = AudioSystem.getAudioInputStream(soundFile);
+                Clip clip = AudioSystem.getClip();
+                clip.open(ais);
+                clip.start();
+            } else {
+                System.out.println("Sound not found: " + filename);
+            }
+        } catch (Exception e) {
+            System.out.println("Error playing sound: " + e.getMessage());
+        }
+    }
+    
+    public static void playButton() {
+        playSound("button_sound.wav");
+    }
+    
+    public static void playAttack() {
+        playSound("attack_sound.wav");
+    }
+    
+    public static void playHeal() {
+        playSound("hpmanaheal_sound.wav");
     }
 }
